4f9c00c63494236229be2bab0bf91a512742f9f5
Dimatuzkoff <dimatuzkoff@gmail.com>
2026-02-01 14:53:25 +0200

    added itenDetail component

--- src/entities/client/ui/ClientListItemDetail/ClientListItemDetail.tsx
+++ src/entities/client/ui/ClientListItemDetail/ClientListItemDetail.tsx
@@ -1,29 +1,57 @@
 // react
-import { useParams } from 'react-router';
+import type { FC, ChangeEvent } from 'react';
+import { useState } from 'react';
+// hooks
+import { useDeleteClientMutation, useUpdateClientMutation } from '@/entities/client/api/clientAPI';
+// types
+import type { IClient } from '@/entities/client/model/types/client'
 // styles
 import styles from './ClientListItemDetail.module.scss';  
-// hooks
-import { useGetClientQuery } from '@/entities/client/api/clientAPI';
 
-export const ClientListItemDetail= ({
-      
+interface ClientListItemDetail {
+    client: IClient
+}
+
+export const ClientListItemDetail: FC<ClientListItemDetail> = ({
+    client: { _id, name, phone, email }   
 }) => {
-    const { id } = useParams<{id: string}>();
-    if (!id) {
-        return <div>Client ID not found</div>;
-    }
-    const { data, isError, isLoading } = useGetClientQuery(id);
+    const [deleteClient, { isSuccess, isError}] = useDeleteClientMutation()
+    const [updateClient, { isSuccess: isSuccessUpdate, isError: isErrorUpdate}] = useUpdateClientMutation()
+    const [currentName, setCurrentName] = useState(name ?? "");
+    const [currentPhone, setCurrentPhone] = useState(phone ?? "");
+    const [currentEmail, setCurrentEmail] = useState(email ?? "");
 
-    if (isError) {
-        return <div>Something went wrong!</div>;
-    }
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+        const onChangeName = (e: ChangeEvent<HTMLInputElement>) => setCurrentName(e.target.value);
+        const onChangePhone = (e: ChangeEvent<HTMLInputElement>) => setCurrentPhone(e.target.value);
+        const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => setCurrentEmail(e.target.value);
+
+        const onDeleteClient = () => { deleteClient(_id).unwrap().then(() => {
+        if (isSuccess) console.log('client delete!!');
+        if (isError) console.log('delete error'); 
+    }) };
+
+    const onUpdateClient = () => { updateClient({
+        _id,
+        name: currentName,
+        phone: currentPhone,
+        email: currentEmail
+    }).unwrap().then(() => {
+        if (isSuccessUpdate) console.log('client updated!!');
+        if (isErrorUpdate) console.log('updated error'); 
+    })}
     return(
         <>
             <div className={styles.client}>
-                <h2> {id} </h2>
+                <h2>Client id № {_id} </h2>
+                <div className={styles.clientInfo}>
+                    <input type="text" value={currentName} onChange={onChangeName} />
+                    <input type="text" value={currentPhone} onChange={onChangePhone} />
+                    <input type="text" value={currentEmail} onChange={onChangeEmail} />
+                    <div className={styles.clientActions}>
+                        <button onClick={onUpdateClient} type="button">Edit</button>
+                        <button onClick={onDeleteClient} type="button">🗑️</button>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/pages/clientPage/ui/ClientPage/ClientPage.tsx
+++ src/pages/clientPage/ui/ClientPage/ClientPage.tsx
@@ -1,4 +1,26 @@
+// react
+import { useParams } from 'react-router';
+// hooks
+import { useGetClientQuery } from '@/entities/client/api/clientAPI';
 // components
 import { ClientListItemDetail } from '@/entities/client/ui/ClientListItemDetail/ClientListItemDetail'
+// styles
+import styles from './ClientPage.module.scss';
+// assets
+import Loader from '@/shared/libs/assets/svg/preview.gif'
 
-export const ClientPage = () => <ClientListItemDetail />
+export const ClientPage = () => {
+    const { id } = useParams<{id: string}>();
+    if (!id) {
+        return <div className={styles.clientPage}>Client ID not found</div>;
+    }
+    const { data, isError, isLoading } = useGetClientQuery(id);
+    if (isError) {
+        return <div className={styles.clientPage}>Something went wrong!</div>;
+    }
+    return (
+        <div className={styles.clientPage}>
+            { isLoading ? (<img src={Loader} alt='loader' />) : <ClientListItemDetail client={data}/>}
+        </div>
+    )
+} 

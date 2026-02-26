d619342beb1e250228565c5136ada3a0eba5efa6
Dimatuzkoff <dimatuzkoff@gmail.com>
2026-02-26 17:00:32 +0200

    added profile component

---
+++ src/entities/user/ui/UserProfile/UserProfile.tsx
@@ -0,0 +1,25 @@
+// hooks
+import { useGetUserInfoQuery } from "@/entities/user/api/userApi"
+// styles
+import styles from './UserProfile.module.scss';
+// assets
+import Loader from '@/shared/libs/assets/svg/preview.gif'
+
+export const UserProfile = () => {
+    const { data, isError, isLoading } = useGetUserInfoQuery();
+
+    if (isError) {
+        return <div>Something went wrong!</div>;
+    }
+    if (isLoading) {
+        return <div><img src={Loader} alt='loader' /></div>;
+    }
+    return (
+        <>
+            <main className={styles.userProfile}>
+                <h1>USER Profile</h1>
+            </main>
+        </>
+        
+    );
+};
--- src/pages/profile/ui/ProfilePage.tsx
+++ src/pages/profile/ui/ProfilePage.tsx
@@ -1,12 +1,12 @@
 // ui
-
+import { UserProfile } from '@/entities/user/ui/UserProfile/UserProfile'
 // styles
 import styles from './ProfilePage.module.scss';
 
 export const ProfilePage = () => {
     return (
         <main className={styles.profile}>
-            <h1>Profile</h1>
+            <UserProfile />
         </main>
     );
 };
